d5583f814b0a0128e9ac5690ca2f1547fb78f6d2
Mattias Fält <faldt.mattias@gmail.com>
2016-05-03 15:04:02 +0200

    Fixed requirements

--- src/ControlExamplePlots.jl
+++ src/ControlExamplePlots.jl
@@ -1,12 +1,5 @@
 module ControlExamplePlots
 
-try
-    Pkg.installed("VisualRegressionTests")
-catch
-    warning("VisualRegressionTests needs to be installed to test plots, adding now:")
-    Pkg.clone("https://github.com/tbreloff/VisualRegressionTests.jl.git")
-end
-
 using ControlSystems, Plots, VisualRegressionTests
 
 include("genplots.jl")
